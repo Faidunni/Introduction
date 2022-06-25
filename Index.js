9d2e16ba938949e6532954720a8a54cfb1a74c07
Faizat Hazeez <99651410+Faidunni@users.noreply.github.com>
2022-06-25 01:23:58 +0100

    Initial commit

---
+++ Index.js
@@ -0,0 +1,9 @@
+var myName = "Hazeez Faizat ";
+var height = 143;
+var country = "Nigeria";
+
+document.write ("My name is " + myName);
+document.write("<br>")
+document.write ("My name is " + height + "cm");
+document.write("<br>")
+document.write ("My name is " +ncountry);
